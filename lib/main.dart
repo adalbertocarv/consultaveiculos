2a499aa78b64c57b351bdf5ed6c00d86fdeaba19
adalbertocarv <adalbertosantu@gmail.com>
2024-06-11 10:44:53 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,52 +2,67 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+// Função para obter o status do veículo a partir do servidor
 Future<String?> obterStatusVeiculo(String placaVeiculo) async {
   try {
-    final response = await http.get(Uri.parse('http://seuipv4:3000/vehicle/$placaVeiculo'));
+    // Faz uma requisição GET para a URL fornecida, substituindo a placa do veículo na URL
+    final response = await http.get(Uri.parse('http://10.63.32.45:3000/vehicle/$placaVeiculo'));
 
+    // Verifica o código de status da resposta
     if (response.statusCode == 200) {
+      // Se a resposta for bem-sucedida, decodifica o corpo da resposta
       final data = json.decode(response.body);
-      return data['status'];
+      return data['status']; // Retorna o status do veículo
     } else if (response.statusCode == 404) {
-      return 'Veículo não encontrado';
+      return 'Veículo não encontrado'; // Retorna mensagem se o veículo não for encontrado
     } else {
-      return 'Erro ao consultar o banco de dados';
+      return 'Erro ao consultar o banco de dados'; // Retorna mensagem de erro genérico
     }
   } catch (e) {
+    // Captura e imprime qualquer erro que ocorra durante a solicitação
     print('Erro ao fazer a solicitação: $e');
-    return 'Erro ao fazer a solicitação';
+    return 'Erro ao fazer a solicitação'; // Retorna mensagem de erro em caso de exceção
   }
 }
 
+// Função principal que inicia o aplicativo Flutter
 void main() {
   runApp(MyApp());
 }
 
+// Classe principal do aplicativo
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Define o tema do aplicativo
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      // Define a tela inicial do aplicativo
       home: HomeScreen(),
     );
   }
 }
 
+// Classe do widget da tela inicial
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
+// Estado associado ao widget da tela inicial
 class _HomeScreenState extends State<HomeScreen> {
+  // Controlador para o campo de texto
   final TextEditingController _placaController = TextEditingController();
+  // Variável para armazenar o status do veículo
   String? _statusVeiculo;
 
+  // Função para consultar o status do veículo
   Future<void> _consultarStatus() async {
     var status = await obterStatusVeiculo(_placaController.text.trim());
+    // Atualiza o estado da tela com o status do veículo
     setState(() {
       _statusVeiculo = status ?? "Erro ao consultar o banco de dados";
     });
@@ -61,17 +76,20 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // Exibe a imagem do ônibus no topo
             Image.asset(
               'images/onibus.png', // Certifique-se de ter uma imagem de ônibus em assets/bus.png
               height: 300,
             ),
             SizedBox(height: 20),
+            // Título do campo de pesquisa
             Text(
               '🔍 Pesquise o status do ônibus',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.blueAccent),
               textAlign: TextAlign.center,
             ),
             SizedBox(height: 20),
+            // Campo de entrada de texto para a placa do veículo
             TextField(
               controller: _placaController,
               decoration: InputDecoration(
@@ -83,6 +101,7 @@ class _HomeScreenState extends State<HomeScreen> {
               style: TextStyle(fontSize: 24),
             ),
             SizedBox(height: 20),
+            // Botão para acionar a consulta
             ElevatedButton(
               onPressed: _consultarStatus,
               style: ElevatedButton.styleFrom(
@@ -92,6 +111,7 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Text('Consultar 🔍'),
             ),
             SizedBox(height: 20),
+            // Exibe o status do veículo, se disponível
             if (_statusVeiculo != null) ...[
               Text(
                 'Status: $_statusVeiculo',
